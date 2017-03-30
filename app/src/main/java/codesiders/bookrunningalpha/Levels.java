a9230432ceae534437742670801d5127bd47d764
adrihanx <adrihanx@gmail.com>
2017-03-29 18:31:46 -0600

    Cree archivo levels. Modifiación concesionaria

--- app/src/main/java/codesiders/bookrunningalpha/Levels.java
+++ app/src/main/java/codesiders/bookrunningalpha/Levels.java
@@ -10,12 +10,25 @@ public class Levels {
     private String Objetivo;
     private int id_Historia;
 
-    public int getId_Historia() {
-        return id_Historia;
+    public Levels(String objetivo, int vel_Minima, int no_Nivel) {
+        Objetivo = objetivo;
+        Vel_Minima = vel_Minima; 
+        No_Nivel = no_Nivel;
     }
 
-    public void setId_Historia(int id_Historia) {
+    public Levels(int id_Historia, int no_Nivel, int vel_Minima, String objetivo) {
         this.id_Historia = id_Historia;
+        No_Nivel = no_Nivel;
+        Vel_Minima = vel_Minima;
+        Objetivo = objetivo;
+    }
+
+    public int getNo_Nivel() {
+        return No_Nivel;
+    }
+
+    public void setNo_Nivel(int no_Nivel) {
+        No_Nivel = no_Nivel;
     }
 
     public int getVel_Minima() {
@@ -34,11 +47,11 @@ public class Levels {
         Objetivo = objetivo;
     }
 
-    public int getNo_Nivel() {
-        return No_Nivel;
+    public int getId_Historia() {
+        return id_Historia;
     }
 
-    public void setNo_Nivel(int no_Nivel) {
-        No_Nivel = no_Nivel;
+    public void setId_Historia(int id_Historia) {
+        this.id_Historia = id_Historia;
     }
 }
--- concecionaria.sql
+++ concecionaria.sql
@@ -9,9 +9,9 @@ create database BookRunning;
 		Estado varchar(30),
 		surs_Imagen varchar(30) default './default.jpg',
 		Peso int,
+		Contrasenia enum('sha1','md5'),
 		primary key (Nombre_Usuario)
 	);
-
 	create table Historia(
 		id_Historia int,
 		Duracion time,
@@ -22,7 +22,6 @@ create database BookRunning;
 		primary key (id_Historia)
 	);
 
-
 	create table Nivel(
 		No_Nivel int,
 		Vel_Minima int,
@@ -32,7 +31,7 @@ create database BookRunning;
 		foreign key(id_Historia) references Historia(id_Historia)
 	);
 
-	create table Rejistro(
+	create table Registro(
 		id_Registro int,
 		Nombre_Usuario varchar(30),
 		Fecha date,
@@ -43,7 +42,7 @@ create database BookRunning;
 		Cal_Quemadas int,
 		primary key(id_Registro),
 		foreign key (Nombre_Usuario) references Usuario(Nombre_Usuario),
-		foreign key (No_Nivel) references Juego(No_Nivel)
+		foreign key (No_Nivel) references Nivel(No_Nivel)
 	);
 
 	create table Gratis(
@@ -58,13 +57,17 @@ create database BookRunning;
 		Foreign key(Id_Historia) references Historia(id_Historia)
 	);
 	create table Bitacora(
-		Id_Bitacora varchar(20)
+		Id_Bitacora varchar(20),
+		NombredeUsuario varchar(30) default 'no root',
+		Accion varchar(20),
+		ValorME varchar(20)
 	);
-insert into Usuario values('Tabo style','tabo_style@outlook',43,'México','Qro',default,78);
-insert into Usuario values('Laura Marin','Laura123@outlook',26,'México','Qro',default,59);
-insert into Usuario values('Daniel Macotela','Daniel_117@outlook',19,'México','Qro',default,80);
-insert into Usuario values('Adrihanx','Adrihanx@outlook',30,'México','Qro',default,74);
-insert into Usuario values('Gerardo San','gerardo@outlook',22,'México','Qro',default,78);
+
+insert into Usuario values('Tabo style','tabo_style@outlook',43,'México','Qro',default,78,'sha1');
+insert into Usuario values('Laura Marin','Laura123@outlook',26,'México','Qro',default,59,'sha1');
+insert into Usuario values('Daniel Macotela','Daniel_117@outlook',19,'México','Qro',default,80,'sha1');
+insert into Usuario values('Adrihanx','Adrihanx@outlook',30,'México','Qro',default,74,'sha1');
+insert into Usuario values('Gerardo San','gerardo@outlook',22,'México','Qro',default,78,'sha1');
 
 insert into Historia values(3743,'3:31','Terror','Guillermo.G',default,default);
 insert into Historia values(4152,'10:02','Suspenso','Andre.R',default,default);
@@ -72,14 +75,14 @@ insert into Historia values(5248,'8:32','Suspenso','Roberto.D',default,default);
 insert into Historia values(3409,'12:50','Romantica','Angelica.M',default,default);
 insert into Historia values(3710,'13:09','terror','Emilio.C',default,default);
 
-insert into Juego values(4,1.3,default,3743);
-insert into Juego values(7,1.5,default,4152);
-insert into Juego values(2,2.25,default,5248);
-insert into Juego values(3,2.07,default,3409);
-insert into Juego values(6,3.47,default,3710);
+insert into Nivel values(4,1.3,default,3743);
+insert into Nivel values(7,1.5,default,4152);
+insert into Nivel values(2,2.25,default,5248);
+insert into Nivel values(3,2.07,default,3409);
+insert into Nivel values(6,3.47,default,3710);
 
-insert into Rejistro values(15345,'Tabo style','03-03-2017',3,4,1.3,10,300);
-insert into Rejistro values(13784,'Laura Marin','12-03-2017',2,7,1.5,8.5,320);
-insert into Rejistro values(13783,'Daniel Macotela','21-01-2017',5,2,2.35,9.5,200);
-insert into Rejistro values(13782,'Adrihanx','15-03-2017',1,3,2.07,7.92,100);
-insert into Rejistro values(15344,'Gerardo San','08-02-2017',6,6,3.42,12,300);
+insert into Registro values(15345,'Tabo style','03-03-2017',3,4,1.3,10,300);
+insert into Registro values(13784,'Laura Marin','12-03-2017',2,7,1.5,8.5,320);
+insert into Registro values(13783,'Daniel Macotela','21-01-2017',5,2,2.35,9.5,200);
+insert into Registro values(13782,'Adrihanx','15-03-2017',1,3,2.07,7.92,100);
+insert into Registro values(15344,'Gerardo San','08-02-2017',6,6,3.42,12,300);
